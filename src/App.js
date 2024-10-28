50841be3e17a7368e0b1fd51275d3363c0b5ad05
CongKony <chicong442004@gmail.com>
2024-10-28 15:19:24 +0700

    hehehehehe

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import '~/App.css';
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 function App() {
@@ -6,22 +6,22 @@ function App() {
   const [posts, setPosts] = useState([])
   const [width, setWidth] = useState(window.innerWidth);
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(res => res.json())
-      .then(posts => {
-        setPosts(posts);
-      })
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/posts")
+  //     .then(res => res.json())
+  //     .then(posts => {
+  //       setPosts(posts);
+  //     })
 
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-    }
-    window.addEventListener('resize', handleResize);
+  //   const handleResize = () => {
+  //     setWidth(window.innerWidth);
+  //   }
+  //   window.addEventListener('resize', handleResize);
 
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    }
-  }, [])
+  //   return () => {
+  //     window.removeEventListener('resize', handleResize);
+  //   }
+  // }, [])
 
 
 
